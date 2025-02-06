fb4a8793a676f7d37c318acff87e7943eb3ecd18
Juan Felipe Ledesma Velasquez <111008847+JuanFelipeLedesma@users.noreply.github.com>
2025-02-06 15:45:35 -0500

    si

--- monitoring/models.py
+++ monitoring/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Variable(models.Model):
+class Measurement(models.Model):
     name = models.CharField(max_length=100)
     value = models.FloatField()
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return f"{self.name} - {self.value}"
